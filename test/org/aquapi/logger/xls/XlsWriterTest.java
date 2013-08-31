01d02df63e46de4f4b5b7ffecc6be84ad71ffd7f
David Michel <dmichel76@googlemail.com>
2013-08-31 20:05:09 +0100

    added basic xls editing

--- test/org/aquapi/logger/xls/XlsWriterTest.java
+++ test/org/aquapi/logger/xls/XlsWriterTest.java
@@ -1,8 +1,17 @@
 package org.aquapi.logger.xls;
 
+import jxl.read.biff.BiffException;
+import jxl.write.WriteException;
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.fail;
 
 /**
  * Unit tests for {@link XlsWriter}
@@ -10,18 +19,38 @@ import java.io.File;
 public class XlsWriterTest
 {
 
-    @Test
-    public void should_be_able_to_create_a_new_xls_file()
-    {
-        try
-        {
-            XlsWriter writer = new XlsWriter();
-            writer.createNew(new File("test.xls"));
-        }
-        catch(Exception ex)
-        {
+    private static File newXlsFile;
 
-        }
+    @BeforeClass
+    public static void setUp()
+    {
+        newXlsFile = new File("new.xls");
+    }
+
+    @AfterClass
+    public static void tearDown()
+    {
+        FileUtils.deleteQuietly(newXlsFile);
+    }
+
+    @Test
+    public void should_be_able_to_create_a_new_xls_file() throws IOException, WriteException
+    {
+        XlsWriter writer = new XlsWriter(newXlsFile);
+        writer.create();
+
+        assert(newXlsFile.exists());
+    }
+
+    @Test
+    public void should_be_able_to_write_to_existing_file() throws URISyntaxException, IOException, WriteException, BiffException
+    {
+        File existingXlsFile = new File(this.getClass().getResource("existing.xls").toURI());
+        if(!existingXlsFile.exists()) fail("test data not found");
+
+        XlsWriter writer = new XlsWriter(existingXlsFile);
+        writer.editValue("Sheet1", 2, 2, 3.5);
+
     }
 
 }
