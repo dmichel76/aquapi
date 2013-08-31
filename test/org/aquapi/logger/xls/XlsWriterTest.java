ab63b095dd1eb4c98b181b507ba540c95ac322bf
David Michel <dmichel76@googlemail.com>
2013-08-31 16:00:43 +0100

    added unit tests

---
+++ test/org/aquapi/logger/xls/XlsWriterTest.java
@@ -0,0 +1,11 @@
+package org.aquapi.logger.xls;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 31/08/13
+ * Time: 15:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class XlsWriterTest {
+}
